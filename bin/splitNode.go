fe38d4fc628854c7802f339b71a0d348899090c6
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-05 18:54:13 +0300

    split node: wip 2

--- bin/splitNode.go
+++ bin/splitNode.go
@@ -2,18 +2,50 @@ package main
 
 //оператор внешнего выбора -External Choice
 type SplitDispatcher interface{
-	Write(in Set) 
+	WriteWithChoice(in Set) 
 }
 
 
 //оператор чередования Interleaving
 type SplitBuffer interface {
-	Queue(out Set)
+	Interleave(out Set)
 }
 
 //параллельная композиция
 type SplitNode interface{
-	Write(in Set)
-	Queue(out Set)
+	WriteWithChoice(in Set)
+	Interleave(out Set)
 	
 }
+
+
+type LocalSplitDispatcher struct{
+	currentIndex int;
+	channelsNumber int;
+	InChan chan Set
+	BoxesChans chan[] Set
+	//Wire который надо передать сорсу
+	
+}
+
+func (lsd *LocalSplitDispatcher) WriteWithChoice (in Set, boxesChans chan[] Set){
+	//take current index пиши по модулю 
+}
+
+type LocalSplitBuffer struct{
+	buffer []Set
+	SinkChan Wire
+	
+}
+
+func (lsp *LocalSplitBuffer) Interleave(out Set){
+	// push all to the buffer the buffer is somehow connected to Wire
+}
+
+type LocalSplitNode struct{
+	Dispatcher LocalSplitDispatcher
+	Nodes []ComputeNode
+	Buffer LocalSplitBuffer
+}
+
+//func (lsn *LocalSplitNode) Writer (Set) 
